f0a4cdbba564c496f9f75396b3310daa97098968
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-07 07:12:19 -0700

    Printing the string _putchar; edit header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print a string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}
