e06096eec469daa1a984f809c81f0a050ddedc6b
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-09 13:44:23 -0700

    FizzBuzz - Edit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,22 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
+		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 		else if (num % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (num % 3 == 0 && num % 5 == 0)
+		else if (num % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 		else
+		{
 			printf("%d ", num);
+		}
 	}
 	printf("\n");
 	return (0);
