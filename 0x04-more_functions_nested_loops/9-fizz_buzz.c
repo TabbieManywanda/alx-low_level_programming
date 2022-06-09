045887037ac475fb00c725e07cad715948bc7e84
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-09 13:46:40 -0700

    FizzBuzz - Edit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (num % 5 == 0)
-		{
-			printf("Buzz ");
-		}
 		else if (num % 3 == 0)
 		{
 			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
