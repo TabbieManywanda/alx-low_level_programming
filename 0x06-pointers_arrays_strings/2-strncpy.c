5780c8001b3b614b2c9e07a4036189c30c190d0e
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-15 08:21:25 -0700

    Edit - Copying n bytes of a string to another string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; *(src + i) != '\0'; i++)
-		;
-
-	while (i < n)
+	for (i = 0; *(src + i) != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	dest[i] = '\0';
 
