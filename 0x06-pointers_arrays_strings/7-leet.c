e7711f144f96d58d866930d48e02a6e3fb2d0468
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-16 03:27:47 -0700

    Edit - Encoding a string into 1337 using one if statement and 2 loops

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,29 +7,18 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+			}
 		}
 	}
 	return (str);
