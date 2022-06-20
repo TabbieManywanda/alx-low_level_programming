a4215adf5bf42b0b6bd019f6d79284f2dced88c8
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-20 14:29:45 -0700

    Prints sum of two diagonals of a matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,14 +3,14 @@
 /**
  *print_diagsums - prints sum of two diagonals of a matrix
  *@a: 2D array
- *size: size of array
+ *@size: size of array
  *
  * Return: void
  */
 void print_diagsums(int *a, int size)
 {
 	int i, sum1, sum2;
-	
+
 	sum1 = sum2 = 0;
 
 	for (i = 0; i < size; i++)
