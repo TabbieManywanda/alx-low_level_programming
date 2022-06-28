7380fdfe9dce1719d6ffd86a6baee9a393295e8b
TabbieManywanda <achiengtabitha@gmail.com>
2022-06-28 11:55:23 -0700

    Concatenates all arguments of a program, each followed by \n

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ *argstostr - Concatenates all arguments of program
+ *@ac: argument count
+ *@av: argument vector
+ *
+ *Return: str or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, size;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	size++;
+
+	str = malloc(size * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
