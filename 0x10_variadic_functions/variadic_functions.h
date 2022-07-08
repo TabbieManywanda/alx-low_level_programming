34ee375afc2f3c3187fd8f8c60a9bb538a0bb479
TabbieManywanda <achiengtabitha@gmail.com>
2022-07-08 12:38:50 -0700

    Printing all numbers - Edit

--- 0x10_variadic_functions/variadic_functions.h
+++ 0x10_variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef VARIADIC
-#define VARIADIC
+#ifndef VARIA_H
+#define VARIA_H
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
