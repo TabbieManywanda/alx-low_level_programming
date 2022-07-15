db080ec0f75906b04957a2a25214ebb25e300f2f
TabbieManywanda <achiengtabitha@gmail.com>
2022-07-15 04:49:43 -0700

    Printing before the main function is executed - Edit punctuation

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 void bmain(void) __attribute__ ((constructor));
 void bmain(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
