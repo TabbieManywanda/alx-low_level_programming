1bd8abd36c4f4268ff25ed4c73982aa6b9357bbb
TabbieManywanda <achiengtabitha@gmail.com>
2022-07-19 11:06:04 -0700

    Deletes node at index of a list - Edit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,6 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 		if (current == NULL)
 			return (-1);
+		i++;
 	}
 	previous = current->next;
 	current->next = previous->next;
