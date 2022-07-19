f7dd8adca1c5bfacafd76b31d2236b4d2a810966
TabbieManywanda <achiengtabitha@gmail.com>
2022-07-19 11:37:07 -0700

    Reverses a linked list - Edit

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,11 +9,8 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *previous;
-	listint_t *next;
-
-	previous = NULL;
-	next = NULL;
+	listint_t *previous = NULL;
+	listint_t *next = NULL;
 
 	while (*head != NULL)
 	{
