a4f8724022e68a4a7bdda86e41283cb5762ea438
TabbieManywanda <achiengtabitha@gmail.com>
2023-01-05 02:23:38 -0800

    Edit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,10 @@
 #ifndef _SEARCH_ALGOS
 #define _SEARCH_ALGOS
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
